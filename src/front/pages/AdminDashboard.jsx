35ea7485165bdc4da4cc2854b83edeea973c73f3
OsorioDanny <daosva97@gmail.com>
2025-08-29 17:42:21 +0000

    reinicio admin

--- src/front/pages/AdminDashboard.jsx
+++ src/front/pages/AdminDashboard.jsx
@@ -1,6 +1,21 @@
 export const AdminDashboard = () => {
     return (
         <div className="container-md">
+            
+            <ul class="nav flex-column">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Active</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
             <h1>Admin Dashboard</h1>
             <p>Welcome to the admin dashboard. Here you can manage the application.</p>
         </div>
