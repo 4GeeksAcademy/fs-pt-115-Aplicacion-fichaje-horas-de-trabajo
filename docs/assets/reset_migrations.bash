3ff744a3561b5a8ac090239ba8b365984abfbc23
daniap03 <danielaunonpardo@gmail.com>
2025-09-07 19:00:41 +0000

    JWT

--- docs/assets/reset_migrations.bash
+++ docs/assets/reset_migrations.bash
@@ -5,10 +5,3 @@ createdb -h localhost -U gitpod example || true &&
 psql -h localhost example -U gitpod -c 'CREATE EXTENSION unaccent;' || true &&
 pipenv run migrate &&
 pipenv run upgrade
-rm -R -f ./migrations &&
-pipenv run init &&
-dropdb -h localhost -U gitpod example || true &&
-createdb -h localhost -U gitpod example || true &&
-psql -h localhost example -U gitpod -c 'CREATE EXTENSION unaccent;' || true &&
-pipenv run migrate &&
-pipenv run upgrade
