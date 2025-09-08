7817a6a1f7040e99f6c84ad0eec5644a3e013340
Nadev <120123131+Nadee23@users.noreply.github.com>
2025-09-08 19:50:30 +0000

    E

--- src/front/pages/SignUp.jsx
+++ src/front/pages/SignUp.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import { crearUsuario } from "../services/APIServices.js";
 
 export const SignUp = () => {
   const [newUser, setNewUser] = useState({
     firstName: "",
     surname: "",
     lastName: "",
+    birthDate: "",
     address: "",
     rol: "",
     email: "",
@@ -19,19 +21,39 @@ export const SignUp = () => {
     setNewUser({ ...newUser, [e.target.name]: e.target.value });
   };
 
+  const [showAlert, setShowAlert] = useState(false);
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    
+    if (
+      newUser.firstName === "" &&
+      newUser.surname === "" &&
+      newUser.email === "" &&
+      newUser.password === "" &&
+      newUser.rol === "" &&
+      newUser.dni_nie === "" &&
+      newUser.iban === "" &&
+      newUser.address === "" &&
+      newUser.lastName === "" &&
+      newUser.birthDate === ""
+    ) {
+      setShowAlert(true);
+      setTimeout(() => {
+        setShowAlert(false);
+      }, 3000);
+      return;
+    }
 
-  }
+    crearUsuario(newUser);
+  };
 
   return (
     <>
       <div>
         <div className="container">
           <div className="row justify-content-center align-content-top min-vh-100 mt-5 bg">
-            <div className="col-md-5">
+            <div className="col-md-8 col-lg-6 col-sm-10">
               <form className="card p-4 shadow rounded-4 bg-dark text-white">
                 <div className="d-flex align-items-center justify-content-center mb-3"></div>
                 <h2 className="text-center mb-3">Sign In</h2>
@@ -55,6 +77,8 @@ export const SignUp = () => {
                     <input
                       type="text"
                       className="form-control"
+                      onChange={handleInputsChange}
+                      value={newUser.firstName}
                       aria-label="Sizing example input"
                       aria-describedby="inputGroup-sizing-sm"
                     />
@@ -69,6 +93,8 @@ export const SignUp = () => {
                     <input
                       type="text"
                       className="form-control"
+                      onChange={handleInputsChange}
+                      value={newUser.surname}
                       aria-label="Sizing example input"
                       aria-describedby="inputGroup-sizing-sm"
                     />
@@ -83,8 +109,21 @@ export const SignUp = () => {
                     <input
                       type="text"
                       className="form-control"
+                      onChange={handleInputsChange}
+                      value={newUser.lastName}
                       aria-label="Sizing example input"
                       aria-describedby="inputGroup-sizing-sm"
+                    />
+                  </div>
+                  {/* Fecha de nacimiento*/}
+                  <div className="input-group input-group-sm mb-3">
+                    <input
+                      type="date"
+                      className="form-control"
+                      onChange={handleInputsChange}
+                      value={newUser.birthDate}
+                      id="fecha"
+                      name="fecha"
                     />
                   </div>
                   <div className="input-group input-group-sm mb-3">
@@ -97,6 +136,8 @@ export const SignUp = () => {
                     <input
                       type="text"
                       className="form-control"
+                      onChange={handleInputsChange}
+                      value={newUser.address}
                       aria-label="Sizing example input"
                       aria-describedby="inputGroup-sizing-sm"
                     />
@@ -111,6 +152,8 @@ export const SignUp = () => {
                     <input
                       type="text"
                       className="form-control"
+                      onChange={handleInputsChange}
+                      value={newUser.rol}
                       aria-label="Sizing example input"
                       aria-describedby="inputGroup-sizing-sm"
                     />
@@ -125,6 +168,8 @@ export const SignUp = () => {
                     <input
                       type="text"
                       className="form-control"
+                      onChange={handleInputsChange}
+                      value={newUser.email}
                       aria-label="Sizing example input"
                       aria-describedby="inputGroup-sizing-sm"
                     />
@@ -139,6 +184,8 @@ export const SignUp = () => {
                     <input
                       type="text"
                       className="form-control"
+                      onChange={handleInputsChange}
+                      value={newUser.password}
                       aria-label="Sizing example input"
                       aria-describedby="inputGroup-sizing-sm"
                     />
@@ -153,6 +200,8 @@ export const SignUp = () => {
                     <input
                       type="text"
                       className="form-control"
+                      onChange={handleInputsChange}
+                      value={newUser.dni_nie}
                       aria-label="Sizing example input"
                       aria-describedby="inputGroup-sizing-sm"
                     />
@@ -167,13 +216,19 @@ export const SignUp = () => {
                     <input
                       type="text"
                       className="form-control"
+                      onChange={handleInputsChange}
+                      value={newUser.iban}
                       aria-label="Sizing example input"
                       aria-describedby="inputGroup-sizing-sm"
                     />
                   </div>
                 </div>
 
-                <button type="submmit" className="btn btn-primary w-100 mt-3">
+                <button
+                  type="submit"
+                  onSubmit={handleSubmit}
+                  className="btn btn-primary w-100 mt-3"
+                >
                   Submit
                 </button>
               </form>
--- src/front/services/APIServices.js
+++ src/front/services/APIServices.js
@@ -1,3 +1,5 @@
+
+//API EXTERNA PARA OBTENER LA UBICACION
 export const getLocation = async () => {
   const response = await fetch(
     `https://ipgeolocation.abstractapi.com/v1/?api_key=${os.getenv("API_KEY")}`
